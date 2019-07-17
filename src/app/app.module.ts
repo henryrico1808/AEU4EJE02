0486b93c2a32af49af5f6412d39613f231ad7e87
Henry Rico <hericoes@ittepic.edu.mx>
2019-07-16 20:34:14 -0600

    ranking view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BarRatingModule } from 'ngx-bar-rating';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -12,7 +14,9 @@ import { ProductListComponent } from './product-list/product-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BarRatingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,33 +1,11 @@
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  public products = [
-    {
-      name: 'JOHN WICK PARABELLUM',
-      id: 1,
-      description: 'A large phone with one of the best screens',
-      img : 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/john-wick-3-1555403175.jpg?crop=1xw:1xh;center,top&resize=320:*',
-      rank : 1,
-    }, 
-    {
-      name: 'ALADIN',
-      id: 2,
-      description: 'A large phone with one of the best screens',
-      img : 'https://cdnvos.lavoz.com.ar/sites/default/files/styles/width_1072/public/pelicula/1aladdin_1558607773.jpg',
-      rank : 2,
-    },
-    {
-      name: 'EL REY LEON',
-      id: 3,
-      description: 'A large phone with one of the best screens',
-      img : 'https://s3.amazonaws.com/statics3.cinemex.com/movie_posters/UEk9btfS7zLueqH-182x272.jpg',
-      rank : 3,
-    },
-  ];
-
+  
   constructor() { }
 }
